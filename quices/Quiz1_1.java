983294a6b927b30390b62708f00b13fb0b245478
diegorrea1 <123745012+diegorrea1@users.noreply.github.com>
2023-08-17 17:59:57 -0500

    Update Quiz1_1.java

--- quices/Quiz1_1.java
+++ quices/Quiz1_1.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 
-public class Quiz1_1{
-public clas static void main(String[]args){
-Scanner sc=new Scanner(System.in);
-  System.out.print("Escriba un numero n:");
-  int n=sc.nextInt();
-  for(int i=1;i<=n;i++){
-  if (i % 3==0 && i%5==0){
-  System.out.println("Fizzbuzz");
-  }
-    else if(i%3==0){
-    System.out.prinln("fizz");
+  public class Quiz1_1 {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("");
+      int n = sc.nextInt();
+      for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+          System.out.println("fizzbuzz"); 
+        }  
+        else if (i % 3 == 0) {
+          System.out.println("fizz");
+        }
+        else if (i % 5 == 0) {
+           System.out.println("buzz");
+        }
+        else {
+          System.out.println(i);
+        }
     }
-    else if(i%5==0){
-      System.out.println("buzz");
     }
-   
   }
-}
-}
